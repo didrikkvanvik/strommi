3964fd31135764975b7cc91d8180885dbfb9b515
Didrik Kvanvik <didrik.kvanvik@gmail.com>
2025-01-28 09:18:19 +0100

    refactor: add description

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,10 @@ if (now.getHours() >= 13) {
 </script>
 
 <template>
-  <main class="w-full flex-1 h-full max-w-[100rem] mx-auto">
+  <main class="w-full flex-1 h-full max-w-[100rem] mx-auto flex items-center flex-col">
+    <p class="font-medium text-green-500 text-xl md:text-xl self-center italic !mt-8 text-center">
+      Strømmi er en enkel side for å se prisen på strøm i Norge.
+    </p>
     <div class="flex flex-col w-full h-full !mt-10 sm:!mt-22 items-center">
       <Datepicker
         v-model="date"
